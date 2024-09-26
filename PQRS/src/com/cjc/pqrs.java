0536c7679c8a1bcf62223a39d06c8f91972eeb61
aaradhyan <Aaradhya.Nachankar@tapasi.com>
2024-09-26 10:38:24 +0530

    by meee

---
+++ PQRS/src/com/cjc/pqrs.java
@@ -0,0 +1,11 @@
+package com.cjc;
+
+public class pqrs {
+public static void main(String[] args) {
+	System.out.println("M1 Method");
+}
+public void m1()
+{
+	System.out.println("Hello m1 method");
+}
+}
